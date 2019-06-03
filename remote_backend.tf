68c5824c8bf6e59a147b8ec13dfde393bc32e951
Ada Lovelace <ada@example.com>
2019-06-03 21:00:56 +0000

    a word

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "sefff-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "sefff-catapp"
     }
   }
 }
